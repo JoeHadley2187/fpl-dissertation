5af6ecb2c4d6a63bff7940d64ca0831b61384d96
Joe Hadley <jhadley2@sheffield.ac.uk>
2026-04-16 14:49:16 +0100

    Fixed mongo seed and also implemented double(or more) gameweeks

--- models/fixture_difficulty.py
+++ models/fixture_difficulty.py
@@ -1,25 +1,43 @@
+"""
+This model decides which multiplier should be applied to a players xp
+taking into account fixture difficulty and how many fixtures a team has in a
+gameweek
+"""
 
 def fixture_difficulty(mongo,player,gw,xp):
-    next_fixture = mongo.db["all_fixtures"].find_one({
-        "event": gw,
-        "element": player["id"]
-    })
-    if next_fixture is None:
-        history = mongo.db["all_histories"].find_one({
+ """
+ :param mongo: the mongo db
+ :param player: the players whose multiplier is being calculated
+ :param gw: the gameweek in which the players next fixture takes place
+ :param xp: the xp before multiplier is being applied
+ :return: the xp with multipliers applied
+ """
+ difficulty = 0
+ next_fixtures = list(mongo.db["all_fixtures"].find({
+    "event":gw,
+    "element": player["id"]
+    }))
+ if len(next_fixtures) > 0:
+        for f in next_fixtures:
+            difficulty += f["difficulty"]
+        return xp * len(next_fixtures) * (1.6 - (0.2 * difficulty/len(next_fixtures)))
+ else:
+        history = list(mongo.db["all_histories"].find({
             "round": gw,
             "element": player["id"]
-        })
-        if history is None:
+        }))
+        if len(history) == 0:
             return 0
-        next_fixture = mongo.db["fixtures"].find_one({
-            "id": history["fixture"],
-        })
-        if player["team"] == next_fixture["team_h"]:
-            difficulty = next_fixture["team_h_difficulty"]
-        else:
-            difficulty = next_fixture["team_a_difficulty"]
-    else:
-        difficulty = next_fixture["difficulty"]
-    return xp * (1.6 - (0.2 * difficulty))
+        fixture_ids = [h["fixture"] for h in history]
+
+        next_fixtures = list(mongo.db["fixtures"].find({
+            "id": {"$in": fixture_ids}
+        }))
+        for f in next_fixtures:
+            if player["team"] == f["team_h"]:
+                difficulty += f["team_h_difficulty"]
+            else:
+                difficulty += f["team_a_difficulty"]
+        return xp * len(next_fixtures) * (1.6 - (0.2 * difficulty/len(next_fixtures)))
 
 
